145a295eeb8a361ffaa2d82855b357689320fc54
Guilherme Arruda <guilherme_arruda@live.com>
2019-04-06 19:56:23 -0300

    Integração do frontend com o backend e adição de ações

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 
@@ -11,6 +15,14 @@ export default class Todo extends Component {
 
 		this.handleChange = this.handleChange.bind(this)
 		this.handleAdd = this.handleAdd.bind(this)
+		this.handleRemove = this.handleRemove.bind(this)
+
+		this.refresh()
+	}
+
+	refresh() {
+		axios.get(`${URL}?sort=-createdAt`)
+			.then(resp => this.setState({...this.state, description: '', list: resp.data}))
 	}
 
 	handleChange(e) {
@@ -18,7 +30,14 @@ export default class Todo extends Component {
 	}
 
 	handleAdd() {
-		console.log(this.state.description)
+		const description = this.state.description
+		axios.post(URL, { description })
+			.then(resp => this.refresh())
+	}
+
+	handleRemove(todo) {
+		axios.delete(`${URL}/${todo._id}`)
+			.then(resp => this.refresh())
 	}
 
 	render() {
@@ -28,7 +47,8 @@ export default class Todo extends Component {
 				<TodoForm handleAdd={this.handleAdd} 
 					handleChange={this.handleChange}
 					description={this.state.description}/>
-				<TodoList />
+				<TodoList list={this.state.list} 
+					handleRemove={this.handleRemove}/>
 			</div>
 		)
 	}

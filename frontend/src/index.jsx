53f5f257a3da7822ac80cb4f4b20deb2de684033
Guilherme Arruda <guilherme_arruda@live.com>
2019-04-18 13:29:31 -0300

    Aplicação final otimizada com react + redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux'
+
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducers'
 
 //estado da aplicação será este, e os objetos estão no arquivo reducers
-const store = createStore(reducers)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers)
 
 ReactDom.render(
     <Provider store={store}>
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,46 +1,65 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Grid from '../template/grid'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import IconButton from '../template/iconButton'
-import {changeDescription} from './todoActions'
+import {add, changeDescription, search, clear} from './todoActions'
 
-const TodoForm = props => {
-	const keyHandler = (e) => {
+//transformar o componente funcional em classe para chamar o componentWillMount
+
+class TodoForm extends Component {
+	constructor(props){
+		super(props)
+		this.keyHandler = this.keyHandler.bind(this)
+	}
+
+	componentWillMount() {
+		this.props.search()
+	}
+
+	keyHandler (e) {
+		//sintaxe do ES2015: extrair as funções add(), search() e o atributo description de this.props
+		//só tem acesso a esses atributos se fizer o mapeamento lá embaixo
+		const { add, search, description, clear } = this.props
 		if (e.key === 'Enter') {
-			e.shiftKey ? props.handleSearch() : props.handleAdd()
+			e.shiftKey ? search() : add(description)
 		} else if (e.key === 'Escape') {
-			props.handleClear()
+			clear()
 		}
 	}
 
-	return (
-		<div>
-			<div role="form" className="todoForm">
-				<Grid cols="12 9 10">
-					<input id="description" className="form-control" placeholder="Adicione uma tarefa"
-						value={props.description} onKeyUp={keyHandler} onChange={props.changeDescription}></input>
-				</Grid>
-	
-				<Grid cols="12 3 2">
-					<IconButton style="primary" icon="plus" 
-						onClick={props.handleAdd}>
-					</IconButton>
-	
-					<IconButton style="info" icon="search" 
-						onClick={props.handleSearch}>
-					</IconButton>
-	
-					<IconButton style="default" icon="close" 
-						onClick={props.handleClear}>
-					</IconButton>
-				</Grid>
+	//toda classe deve ter um metodo render
+	render() {
+		const { add, search, description, clear } = this.props
+
+		return (
+			<div>
+				<div role="form" className="todoForm">
+					<Grid cols="12 9 10">
+						<input id="description" className="form-control" placeholder="Adicione uma tarefa"
+							value={this.props.description} onKeyUp={this.keyHandler} onChange={this.props.changeDescription}></input>
+					</Grid>
+		
+					<Grid cols="12 3 2">
+						<IconButton style="primary" icon="plus" 
+							onClick={() => add(description)}>
+						</IconButton>
+		
+						<IconButton style="info" icon="search" 
+							onClick={search}>
+						</IconButton>
+		
+						<IconButton style="default" icon="close" 
+							onClick={clear}>
+						</IconButton>
+					</Grid>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
-const mapDispatchToProps = dispatch => bindActionCreators({changeDescription}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({add, changeDescription, search, clear}, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
